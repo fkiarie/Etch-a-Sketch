791715a9cf4117b1e6e1ae83b4524f33a08996fb
kazungu <kazungu61@outlook.com>
2023-07-14 14:14:16 +0300

    logic to create a grid box
    The script creates 16*16 box in a grid and adds a css class to each

---
+++ main.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16 * 16; i++) {
+    const gridSquare = document.createElement('div');
+    gridSquare.classList.add('grid-square');
+    container.appendChild(gridSquare);
+}
