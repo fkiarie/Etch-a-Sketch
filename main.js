6310fdfd938bde53dfe2f944ed82e3288804e114
kazungu <kazungu61@outlook.com>
2023-07-14 21:26:54 +0300

    add button function generateGrid
    The createGrid creates the inital grid. When a user clicks on the button
    the generateGrid function is initiated

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 const container = document.querySelector('.container');
+let gridSize = 16;
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    container.appendChild(gridSquare);
+function createGrid(){
+    container.innerHTML = ''; // clear current grid
+    for (let i = 0; i <  gridSize * gridSize; i++) {
+        
+        const gridSquare = document.createElement('div');  // create grid div
+        gridSquare.classList.add('grid-square'); //add css 
+        container.appendChild(gridSquare); // add grid div to container
+    }
+    
 }
+
+function generateGrid(){
+    const userInput = prompt("Enter number of squares per side(1-100");
+    const newGridSize = parseInt(userInput);
+
+    if(!isNaN(newGridSize) && newGridSize > 0 && newGridSize <=100){
+        gridSize = newGridSize; 
+        createGrid();
+    }else{
+        alert("Enter a number between 1 and 100");
+    }
+}
+createGrid();
